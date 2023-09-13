c287b7e2354225a21cec816cbb955ac20ff41d12
Nisarg0077 <nghosty07@gmail.com>
2023-09-13 16:08:10 +0530

    title-tab-update
    Tab title update.

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
 
-    <title>CRUD APP</title>
+    <title>iNotes</title>
 
 </head>
 
